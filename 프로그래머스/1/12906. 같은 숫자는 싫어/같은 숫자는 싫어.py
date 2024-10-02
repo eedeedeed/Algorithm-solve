45d9662ac72e2ce3a5a2e9c2ff775d57da0fbb88
샬롯 슈슈 <tngus01224@naver.com>
2024-10-02 10:07:30 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 195.39 ms, Memory: 28 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    for i in range(1,len(arr)):
+        
+        if arr[i-1] != arr[i]:
+            answer.append(arr[i-1])
+        if i == len(arr)-1:
+            answer.append(arr[i])
+            
+    return answer
