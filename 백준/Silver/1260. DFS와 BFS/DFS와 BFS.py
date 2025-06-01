311dfd19ac1119cc90a0d0b61a85205930ccd82f
임수현 <tngus01224@naver.com>
2025-06-01 23:20:00 +0900

    [Silver II] Title: DFS와 BFS, Time: 320 ms, Memory: 35676 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,59 +1,52 @@
-# N : 정점 개수
-# M : 간선 개수
-# V : 탐색을 시작할 정점의 번호
-
-# 처음 입력받는게 N,M,V
-# M번만큼 또 입력받음.
-
-# 방문 정점이 여러 개일 때 정점 번호 작은 순으로 방문
-
-# DFS : 깊이우선, 스택,재귀
-# BFS : 너비우선, 큐 사용
-
 from collections import deque
+N, M, V= map(int,input().split())
 
-#첫 줄 입력
-N, M, V = map(int,input().split())
+#깊이우선탐색, 정점방문처리, 출력
 
-#그래프 나타내기
+#인접 리스트 초기화
 graph = [[] for _ in range(N+1)]
 
-#간선 수,관계 입력 받기
+#간선 정보 입력
 for _ in range(M):
-    a, b = map(int,input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+	a, b = map(int,input().split())
+	graph[a].append(b)
+	graph[b].append(a)
+	
+# 작은 번호순 방문을 위한 정렬
+for i in range(1,N+1):
+	graph[i].sort()
+	
+visitedDFS = [False]*(N+1)
+visitedBFS = [False]*(N+1)
 
+#DFS 구현 - 재귀
+def DFS(V,visitedDFS):
+	print(V,end=' ')
+	visitedDFS[V] = True
+	
+	#재귀
+	for i in graph[V]:
+		if not visitedDFS[i]:
+	 		DFS(i,visitedDFS)
+	
+# BFS 구현 - 큐
+def BFS(V,visitedBFS):
+	visitedBFS[V] = True
+	
+	#큐
+	queue = deque([V])
+	
+	while queue : #큐 꺼내서 출력
+		current = queue.popleft()
+		print(current, end = ' ')
+		
+		#방문처리, 큐 삽입
+		for i in graph[current]:
+			if not visitedBFS[i]:
+				visitedBFS[i] = True
+				queue.append(i)
 
-#오름차순 정렬
-for n in range(1,N+1):
-    graph[n].sort()
-
-# 방문처리 리스트 생성
-visitedDFS = [False] * (N+1)
-visitedBFS = [False] * (N+1)
-
-def DFS(V):
-    print(V, end = " ")
-    visitedDFS[V] = True
-
-    for i in graph[V] : # 이 부분에서 깊이 파고든다.
-        if visitedDFS[i] == False :
-            DFS(i)
-
-def BFS(V):
-    visitedBFS[V] = True
-    queue = deque([V])
-
-    while queue :
-        i = queue.popleft()
-        print(i, end= ' ')
-        for g in graph[i]:
-            if visitedBFS[g] == False :
-                visitedBFS[g] = True
-                queue.append(g)
-
-
-DFS(V)
+#DFS 실행
+DFS(V,visitedDFS)
 print()
-BFS(V)
+BFS(V,visitedBFS)
